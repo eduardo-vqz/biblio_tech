385ea6f56ffabdd4aaf4deca20beff3fdc38884e
Eduardo Vásquez <eduardo.vqz968@gmail.com>
2025-11-21 16:41:58 -0600

    Modelo autorLibro

---
+++ app/Models/AutorLibro.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+class AutorLibro
+{
+    private int $id_libro;
+    private int $id_autor;
+
+    public function __construct(
+        int $id_libro,
+        int $id_autor
+    ) {
+        $this->id_libro = $id_libro;
+        $this->id_autor = $id_autor;
+    }
+
+    public function getIdLibro(): int
+    {
+        return $this->id_libro;
+    }
+    public function setIdLibro(int $id_libro): void
+    {
+        $this->id_libro = $id_libro;
+    }
+
+    public function getIdAutor(): int
+    {
+        return $this->id_autor;
+    }
+
+    public function setIdAutor(int $id_autor): void
+    {
+        $this->id_autor = $id_autor;
+    }
+}
